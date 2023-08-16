c0910b391bcc75c40ba303be6de91f78efe08207
Santiago Alvarez Barbosa <56238062+SantiagoAlvarezb@users.noreply.github.com>
2023-08-16 13:18:29 -0500

    Add files via upload

---
+++ gde/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+# Registers various models with the admin interface, allowing administrators
+# to manage and interact with the data stored in these models.
+
+admin.site.register(Store)
+
+admin.site.register(Store_location)
+
+admin.site.register(Category)
+
+admin.site.register(Videogame)
+
+admin.site.register(Format)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Platform)
+admin.site.register(Dlc)
+admin.site.register(Shopping_cart_packages)
+admin.site.register(Shopping_cart)
+admin.site.register(Shopping_cart_videogames)
+admin.site.register(Shopping_cart_dlc)
+
+admin.site.register(Dispatcher)
+admin.site.register(Package)
